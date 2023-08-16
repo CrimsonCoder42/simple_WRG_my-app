dfb5ab378cef2c3626a94ce9d7a8909afba42c34
CrimsonCoder42 <nostro37@gmail.com>
2023-08-16 13:31:26 -0400

    init project setup

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function HelloWorld() {
+  return (
+    <Fragment>
+      <h1>Hello World</h1>
+      <p>React is awesome!</p>
+    </Fragment>
+  );
+}
+
+export default HelloWorld;
